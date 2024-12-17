aaf2e543cd0efc29e37061eb5df9a330a71ee59e
Christian Gonzalez <chrisalmonte@protonmail.com>
2024-12-16 23:50:17 -0600

    Dibujar aristas

--- visualizacion.py
+++ visualizacion.py
@@ -2,6 +2,7 @@
 
 import pgrafos
 import pygame
+import random
 
 #Propiedades del Grafo
 grafo = pgrafos.Grafo.generar_desde_archivo("grafos/malla/malla_30.gv")
@@ -13,7 +14,27 @@ ventana_color = pygame.Color(30,30,30) #RGB
 nodo_color = pygame.Color(220,220,220,255) #RGBA
 nodo_radio = 10 #px
 arista_color = pygame.Color(220,220,220,255)
-arista_ancho = 1.2
+arista_ancho = 1
+
+#Funciones para el programa
+def definir_posiciones_iniciales(grafo):
+    for nodo in grafo.nodos:
+        nodo.definir_propiedad("vis_x", random.randrange(0, ventana_ancho - (nodo_radio * 2)))
+        nodo.definir_propiedad("vis_y", random.randrange(0, ventana_alto - (nodo_radio * 2)))
+
+def calcular_posiciones(grafo):
+    for nodo in grafo.nodos:
+        nodo.definir_propiedad("vis_x", max(0, min(ventana_ancho - (nodo_radio * 2), nodo.propiedad.get("vis_x", 0) + ((random.randrange(-50,50) * delta_time)))))
+        nodo.definir_propiedad("vis_y", max(0, min(ventana_alto - (nodo_radio * 2), nodo.propiedad.get("vis_y", 0) + ((random.randrange(-50,50) * delta_time)))))
+
+def dibujar_grafo(surface, grafo):
+    for arista in grafo.aristas:
+        inicio = (arista.extremos[0].propiedad.get("vis_x", 0) + nodo_radio, arista.extremos[0].propiedad.get("vis_y", 0) + nodo_radio)
+        fin = (arista.extremos[1].propiedad.get("vis_x", 0) + nodo_radio, arista.extremos[1].propiedad.get("vis_y", 0) + nodo_radio)
+        pygame.draw.line(surface, arista_color, inicio, fin, arista_ancho)
+
+    for nodo in grafo.nodos:
+        surface.blit(nodo_sprite, dest=(nodo.propiedad.get("vis_x", 0), nodo.propiedad.get("vis_y", 0)), special_flags=pygame.BLEND_RGBA_ADD)
 
 #Inicializar pygame
 pygame.init()
@@ -27,16 +48,9 @@ nodo_sprite = pygame.Surface((nodo_radio * 2, nodo_radio * 2))
 nodo_sprite.fill((0,0,0,0)) #fondo transparente
 pygame.draw.circle(nodo_sprite, nodo_color, (nodo_sprite.width/2, nodo_sprite.height/2), nodo_radio)
 
-#Funciones para el programa
-def calcular_posiciones(grafo):
-    pass
-
-def dibujar_grafo(surface, grafo):
-    surface.blit(nodo_sprite, dest=(0,0), special_flags=pygame.BLEND_RGBA_ADD)
-    pass
-
-
 #Ejecución del programa
+definir_posiciones_iniciales(grafo)
+
 while ejecutandose:
     #Cerrar programa cuando se presiona "X" en la ventana.
     for event in pygame.event.get():
